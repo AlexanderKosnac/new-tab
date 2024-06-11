af61f20ebac7f35b4209a34aa0da916de8d5e6ba
Alexander Kosnac <alexander.kosnac@web.de>
2024-06-12 00:47:09 +0200

    Scaffolding for popup and data editing

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -1,7 +1,46 @@
 <script>
+  import data from "./lib/data.json";
+
+  let ntdata;
+
+  function save() {
+    console.log(ntdata);
+    browser.storage.local.set({ "new-tab-data": ntdata });
+  }
+
+  function clear() {
+    browser.storage.local.remove("new-tab-data");
+  }
+
+  function restoreDefault() {
+    browser.storage.local.set({ "new-tab-data": data });
+  }
+
+  let dataPromise = browser.storage.local.get("new-tab-data");
+  dataPromise.then(it => { ntdata = it["new-tab-data"] }, () => {});
 </script>
 
-<div style="border: 1px solid black; width: 200px; height: 200px"></div>
+{#await dataPromise}
+  <p>Loading new-tab data from storage.</p>
+{:then _}
+  <div style="display: flex; flex-direction: column; gap: 2px">
+    <div style="display: flex; flex-direction: row; gap: 2px">
+      <button type="button" on:click={save}>Save</button>
+      <button type="button" on:click={restoreDefault}>Default</button>
+      <button type="button" on:click={clear}>Clear</button>
+    </div>
+    <br>
+    <label>
+      Title:
+      <input type="text" bind:value={ntdata["title"]}/>
+    </label>
+  </div>
+
+  <pre>{JSON.stringify(ntdata, null, 2)}</pre>
+{:catch error}
+  <p style="color: red">Could not load new-tab data from storage.</p>
+  <code>{error}</code>
+{/await}
 
 <style>
 </style>
