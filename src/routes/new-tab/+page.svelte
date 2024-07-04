4acc1eeae38edb7288d7379ccd892164442d7f18
Alexander Kosnac <alexander.kosnac@web.de>
2024-07-04 16:19:04 +0200

    Add gear icon to toggle edit mode

--- src/routes/new-tab/+page.svelte
+++ src/routes/new-tab/+page.svelte
@@ -4,6 +4,8 @@
 
   import data from "$lib/data.json";
 
+  let editable = false;
+
   const componentMapping = {
     "header": Header,
     "favorite": Favorite,
@@ -34,7 +36,10 @@
   <!--pre>{JSON.stringify(data, null, 2)}</pre-->
 
   <div class="d-flex align-items-center justify-content-space-around">
-    <span class="title">{d["title"] ?? "New Tab"}</span>
+    <div class="title">{d["title"] ?? "New Tab"}</div>
+    <svg id="btnEdit" xmlns="http://www.w3.org/2000/svg" width="24" height="24" class:editable={editable} stroke="white" viewBox="0 0 16 16" on:click={() => { editable = !editable }}>
+      <path transform="scale(0.9 0.9) translate(1 1)" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+    </svg>
   </div>
   <div class="content">
   {#each d["data"] ?? [] as entry}
@@ -53,5 +58,19 @@
 .title {
   font-size: 32px;
   font-weight: bold;
+}
+#btnEdit {
+  width: 24px;
+  height: 24px;
+  background-color: transparent;
+}
+svg#btnEdit {
+  transition: transform .5s, fill .5s;
+  transform: rotate(0deg);
+  fill: transparent;
+}
+svg#btnEdit.editable {
+  transform: rotate(45deg);
+  fill: white;
 }
 </style>
