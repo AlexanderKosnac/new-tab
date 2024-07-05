f92d6e1bd43a29fe8f1aa01953a4c2a397ab77eb
Alexander Kosnac <alexander.kosnac@web.de>
2024-07-06 00:23:48 +0200

    Allow deleting elements in edit mode

--- src/lib/Favorite/Favorite.svelte
+++ src/lib/Favorite/Favorite.svelte
@@ -16,9 +16,20 @@
         );
         linkEl.click();
     }
+
+    function deleteThis() {
+        $newTabData["data"].splice(idx, 1);
+        $newTabData = $newTabData;
+    }
 </script>
 
 <div class="fav-element" draggable="false">
+    {#if editable}
+    <svg xmlns="http://www.w3.org/2000/svg" class="btn-delete" width="16" height="16" fill="#C80036" viewBox="0 0 16 16" on:click={deleteThis}>
+        <circle cx="50%" cy="50%" r="7" fill="white"/>
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+    </svg>
+    {/if}
   
     <a href="{editable ? "javascript: void(0)" : $newTabData["data"][idx].url}" class="link-element" bind:this={linkEl} draggable="false">
         {#if editable}
@@ -112,5 +123,12 @@
     font-size: small;
     min-width: 40px;
     max-width: 60px;
+}
+.btn-delete {
+    position: relative;
+    top: 4px;
+    margin-top: -16px;
+
+    z-index: 1;
 }
 </style>
--- src/lib/Header/Header.svelte
+++ src/lib/Header/Header.svelte
@@ -3,18 +3,30 @@
 
     export let idx;
     export let editable;
+
+    function deleteThis() {
+        $newTabData["data"].splice(idx, 1);
+        $newTabData = $newTabData;
+    }
 </script>
 
 {#if editable}
-<div class="header" bind:textContent={data.text} contenteditable>{data.text}</div>
+<div class="d-flex align-items-center">
+    <div class="header" bind:textContent={$newTabData["data"][idx].text} contenteditable>{$newTabData["data"][idx].text}</div>
+    <svg xmlns="http://www.w3.org/2000/svg" class="btn-delete" width="16" height="16" fill="#C80036" viewBox="0 0 16 16" on:click={deleteThis}>
+        <circle cx="50%" cy="50%" r="7" fill="white"/>
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+    </svg>
+</div>
 {:else}
 <div class="header">{$newTabData["data"][idx].text}</div>
 {/if}
 
 <style>
 .header {
-  font-size: 24px;
-  font-weight: bold;
-  min-width: 50px;
+    font-size: 24px;
+    font-weight: bold;
+    min-width: 50px;
+    margin-right: 10px;
 }
 </style>
