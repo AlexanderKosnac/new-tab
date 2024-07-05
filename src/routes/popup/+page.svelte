f707d46fc94461b9fa3c05b1392e26a32a93c3c1
Alexander Kosnac <alexander.kosnac@web.de>
2024-07-06 01:19:15 +0200

    Rework popup window after moving edit functionality

--- src/routes/popup/+page.svelte
+++ src/routes/popup/+page.svelte
@@ -1,33 +1,11 @@
 <script>
+  import { onMount } from "svelte";
+
   import data from "$lib/data.json";
+  import { newTabData } from "$lib/storage.js";
 
-  let saveStatus;
-
-  let ntdata;
-
-  let elementTypes = [
-    {
-      display: "Favorite",
-      default: {
-        "type": "favorite",
-        "url": "",
-        "label": "",
-        "shortcut": ""
-      }
-    }, {
-      display: "Header",
-      default: {
-        "type": "header",
-        "text": ""
-      }
-    },
-  ];
-  let selectedElementType = elementTypes[0];
-
-  function resetData() {
-    if (confirm("You are about to reset the data to the default. Do you really want to do this?")) {
-      chrome.storage.local.set({ "new-tab-data": data });
-    }
+  function save() {
+    chrome.storage.local.set({ "new-tab-data": $newTabData });
   }
 
   function exportData() {
@@ -41,7 +19,7 @@
 
     let filename = `new-tab-data-${year}-${month}-${day}-${hours}-${minutes}-${seconds}.json`;
 
-    const text = JSON.stringify(ntdata, null, 2);
+    const text = JSON.stringify($newTabData, null, 2);
     let element = document.createElement("a");
     element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
     element.setAttribute("download", filename);
@@ -54,138 +32,59 @@
   function importData() {
     let data = window.prompt("Insert JSON data to import:", "");
     if (data == null) return;
-    ntdata = JSON.parse(data);
+    newTabData.set(JSON.parse(data));
     save();
   }
 
-  function moveUp(idx) {
-    if (idx == 0) return;
-    let tmp = ntdata["data"][idx-1];
-    ntdata["data"][idx-1] = ntdata["data"][idx];
-    ntdata["data"][idx] = tmp;
+  function resetData() {
+    if (confirm("You are about to reset the data to the default. Do you really want to do this?")) {
+      newTabData.set(data);
+      save();
+    }
   }
 
-  function moveDown(idx) {
-    if (idx == ntdata["data"].length) return;
-    let tmp = ntdata["data"][idx+1];
-    ntdata["data"][idx+1] = ntdata["data"][idx];
-    ntdata["data"][idx] = tmp;
-  }
-
-  function deleteEntry(idx) {
-    ntdata["data"].splice(idx, 1);
-    ntdata["data"] = ntdata["data"];
-  }
-
-  function newEntry() {
-    ntdata["data"].push(selectedElementType.default);
-    ntdata["data"] = ntdata["data"];
-  }
-
-  import { onMount } from 'svelte';
   let dataPromise;
   onMount(() => {
-    dataPromise = chrome.storage.local.set("new-tab-data");
-    dataPromise.then(it => { ntdata = it["new-tab-data"] ?? data }, () => {});
+    dataPromise = chrome.storage.local.get("new-tab-data").then(it => {
+      newTabData.set(it["new-tab-data"] ?? data);
+    });
   });
 </script>
 
 <div class="popup-container">
-  <div>
-  {#await dataPromise}
-    <p>Loading new-tab data from storage.</p>
-  {:then _}
-    {#if ntdata}
-    <div class="d-flex flex-column gap-3">
-      <div class="d-flex flex-row gap-1 align-items-center">
-        <button class="ntinput green" on:click={save}>Save</button>
-        <span bind:this={saveStatus} class="save-status flash-and-hide hidden" style="width: 15ch"></span>
-        <div style="flex-grow: 1"></div>
-        <button class="ntinput" on:click={exportData}>Export</button>
-        <button class="ntinput" on:click={importData}>Import</button>
-        <div style="width: 10px"></div>
-        <button class="ntinput red" on:click={resetData}>Reset</button>
-      </div>
+  <h1>New-Tab Settings</h1>
+  <tt>new-tab</tt> is a plugin that replaces the default "New Tab" page in the browser with a more useful alternative.
+  The project can be found on <a href="https://github.com/AlexanderKosnac/new-tab">GitHub</a>.
 
-      <label>
-        Title:
-        <input type="text" class="ntinput" bind:value={ntdata["title"]}/>
-      </label>
-
-      {#each ntdata["data"] ?? [] as entry, idx}
-      <div class="d-flex flex-row gap-3 align-items-center p-1 data-entry">
-        <div class="d-flex flex-column gap-1">
-          <button class="up-down-arrow" on:click={() => { moveUp(idx) }}>
-            <img src="./icons/arrow.svg" alt="Move up"/>
-          </button>
-          <button class="up-down-arrow" on:click={() => { moveDown(idx) }}>
-            <img src="./icons/arrow.svg" style="transform: rotate(180deg)" alt="Move down"/>
-          </button>
-        </div>
-        {#if entry.type == "header"}
-          <input type="text" class="ntinput flex-grow" size="45" bind:value={entry["text"]}/>
-        {/if}
-        {#if entry.type == "favorite"}
-        <!--img class="icon" alt="Icon" src="{entry["icon"]}" draggable="false" onerror="this.onerror=null; this.src='./icons/placeholder.svg'"/-->
-          <img class="icon" alt="Icon" src="./icons/placeholder.svg" draggable="false" onerror="this.onerror=null; this.src='./icons/placeholder.svg'"/>
-          <input type="text" placeholder="Label" class="ntinput" size="15" bind:value={entry["label"]}/>
-          <input type="text" placeholder="URL" class="ntinput flex-grow" bind:value={entry["url"]}/>
-          <input type="text" placeholder="Shortcut" class="ntinput" maxlength="1" size="5" bind:value={entry["shortcut"]}/>
-        {/if}
-        <button class="ntinput red" on:click={() => { deleteEntry(idx) }}>Delete</button>
-      </div>
-      {/each}
-
-      <div>
-        <select class="ntinput" bind:value={selectedElementType}>
-          {#each elementTypes as type}
-          <option value={type}>{type.display}</option>
-          {/each}
-        </select>
-        <button class="ntinput" on:click={newEntry}>Create Element</button>
-      </div>
+  <h2>Data</h2>
+  <div class="d-flex flex-column gap-3">
+    <div>
+      <button class="ntinput btn-settings" on:click={exportData}>Export</button>
+      Download the stored data as a JSON.
     </div>
-    {/if}
-  {:catch error}
-    <div class="font-error">
-      <p>Failed to load new-tab data from storage.</p>
-      <code>{error}</code>
+    <div>
+      <button class="ntinput btn-settings" on:click={importData}>Import</button>
+      Import a JSON to overwrite the stored data.
     </div>
-  {/await}
+    <div>
+      <button class="ntinput btn-settings" on:click={resetData}>Reset</button>
+      Reset the stored data to the default.      
+    </div>
   </div>
 </div>
 
 <style>
+  a {
+    color: var(--font-color);
+    font-weight: 900;
+    text-decoration: underline;
+  }
   .popup-container {
-    padding: 5px;
+    padding: 0px 5px 0px 5px;
     width: 700px;
+    min-height: 400px;
   }
-  .data-entry {
-    border-left: 6px solid var(--dark-background);
-    background-color: var(--light-background);
-  }
-  .up-down-arrow {
-    background: none;
-    color: inherit;
-    border: none;
-    padding: 0;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
-    line-height: 0;
-  }
-  .up-down-arrow > img {
-    width: 24px;
-    height: 24px;
-  }
-  .icon {
-    border-radius: 7px;
-    min-width:  40px;
-    max-width:  40px;
-    min-height: 40px;
-    max-height: 40px;
-  }
-  .flex-grow {
-    flex-grow: 1;
+  .btn-settings {
+    width: 60px;
   }
 </style>
